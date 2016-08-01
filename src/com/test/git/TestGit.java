1538a6a16afe7bbb93885964bd995d5f9f4204b3
DanceOTS <993672323@qq.com>
2016-08-01 23:03:45 +0800

    3
    3

--- src/com/test/git/TestGit.java
+++ src/com/test/git/TestGit.java
@@ -5,6 +5,7 @@ public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("向服务器发送请求 01");
 		System.out.println("向服务器发送请求 02");
-		System.out.println("服务器发回复 03");	
+		System.out.println("服务器发回复 03");
+		System.out.println("服务器发回复 04");	
 	}
 }
